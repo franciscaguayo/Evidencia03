0fd09f791e8b3171c8018dd85235136c791b38ea
francisca <f.aguayo03@ufromail.cl>
2025-04-10 10:01:33 -0400

    paso 1: dimension de la matriz paso 2: crear la matriz paso 3: llenar matriz

--- src/GestionMatriz.java
+++ src/GestionMatriz.java
@@ -17,9 +17,57 @@ public class GestionMatriz {
         int[][] matriz = crearMatriz(filas, columnas);
 
         llenarMatriz(matriz);
-
-        menuPrincipal(matriz);
-
+        
         System.out.println("Fin del programa");
+    }
+
+    public static boolean validarDimensiones(int filas, int columnas) {
+        if (filas <= 0 || columnas <= 0) {
+            System.out.println("Las dimensiones deben ser mayores que cero");
+            return false;
+        }
+        return true;
+    }
+
+    public static int[][] crearMatriz(int filas, int columnas) {
+        return new int[filas][columnas];
+    }
+
+    public static void llenarMatriz(int[][] matriz) {
+        Random rand = new Random();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = rand.nextInt(10);
+            }
+        }
+        System.out.println("Matriz creada");
+    }
+
+    public static void mostrarFila(int[][] matriz, int fila) {
+        if (fila < 0 || fila >= matriz.length) {
+            System.out.println("Número de fila no válido.");
+            return;
+        }
+
+        System.out.print("Fila " + fila + ": ");
+        for (int j = 0; j < matriz[fila].length; j++) {
+            System.out.print(matriz[fila][j] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean esMatrizCero(int[][] matriz) {
+        int totalElementos = matriz.length * matriz[0].length;
+        int contadorCeros = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == 0) {
+                    contadorCeros++;
+                }
+            }
+        }
+
+        return contadorCeros > totalElementos / 2;
     }
 }
